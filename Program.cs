a2ee4e2787c16641c84cc06b21bfd72f2263e560
Maryem Farooqi <farooqimaryem1@gmail.com>
2020-02-28 11:56:48 -0500

    Console and unit tests completed

--- Program.cs
+++ Program.cs
@@ -10,7 +10,73 @@ namespace MFarooqiA2
     {
         static void Main(string[] args)
         {
+            string menuInput;
+            bool menuContinue = true;
 
+            do
+            {
+                try
+                {
+                    Console.WriteLine("1. Enter triangle dimensions");
+                    Console.WriteLine("2. Exit");
+
+                    menuInput = Console.ReadLine();
+
+
+                    if (menuInput == "2")
+                    {
+                        Environment.Exit(0);
+                        menuContinue = true;
+                    }
+
+                    if (menuInput == "1")
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter a 1st int");
+                            int sideOne = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Please enter a 2nd int");
+                            int sideTwo = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Please enter a 3rd int");
+                            int sideThree = int.Parse(Console.ReadLine());
+
+                            if (TriangleSolver.Analyze(sideOne, sideTwo, sideThree) == "Side cannot be negative")
+                            {
+                                menuContinue = false;
+                            }
+
+                            if (TriangleSolver.Analyze(sideOne, sideTwo, sideThree) == "Side cannot be 0. Please try again")
+                            {
+                                menuContinue = false;
+                            }
+
+                            Console.WriteLine(TriangleSolver.Analyze(sideOne, sideTwo, sideThree));
+                            menuContinue = false;
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Something went wrong: " + ex.Message);
+                            menuContinue = false;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please ONLY enter 1 or 2");
+                        menuContinue = false;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("There was an error: " + ex.Message);
+                    menuContinue = false;
+                }
+
+
+            } while (!menuContinue);
         }
     }
 }
